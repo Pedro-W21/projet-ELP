bc5c75e083add27b06f304283636cc32ff35b17a
Pedro-W21 <89784517+Pedro-W21@users.noreply.github.com>
2025-01-09 00:38:19 +0100

    des fichiers en plus pour le client et le serveur

--- go/main.go
+++ go/main.go
@@ -1,11 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	var image_test Image = MakeImage(1000, 1000, Color{0, 255, 0})
 	fmt.Println(image_test.GetAt(200, 500))
-
 }
 
 type Color struct {
