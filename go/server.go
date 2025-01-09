a7c5e4dd41bd5084fcd354b92caabcd426e4f416
Pedro-W21 <89784517+Pedro-W21@users.noreply.github.com>
2025-01-09 08:42:01 +0100

    début des filtres et du serveur

--- go/server.go
+++ go/server.go
@@ -4,6 +4,33 @@ import (
 	"net"
 )
 
+type ClientData struct {
+	connection         net.TCPConn
+	client_id          uint
+	last_request_id    uint
+	requests_in_flight []ClientRequest
+}
+
+type ClientRequest struct {
+	request_id      uint
+	raw_request     string
+	image           Image
+	filter_name     string
+	raw_filter_data Filter
+}
+
 type TCPServer struct {
-	listener net.TCPListener
+	listener       net.TCPListener
+	last_client_id uint
+	connections    []ClientData
+}
+
+type ClientRequestResponse struct {
+	final_image Image
+	id          uint
+}
+
+type TCPServerSenderThread struct {
+	client_data           ClientData
+	finished_work_channel chan ClientRequestResponse
 }
