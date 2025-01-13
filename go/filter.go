7ff10c9cbeab7bda3710fb2b87078ffa9f695e8a
Pedro-W21 <89784517+Pedro-W21@users.noreply.github.com>
2025-01-13 14:39:35 +0100

    rajout du request_id dans la réponse, et de la méthode PrepareImage dans l'interface

--- go/filter.go
+++ go/filter.go
@@ -2,6 +2,7 @@ package main
 
 type Filter interface {
 	GetPixel(x uint, y uint, image Image) Color
+	PrepareImage(image Image, y_min uint, y_max uint)
 }
 
 type Gaussian struct {
@@ -12,6 +13,10 @@ func (g Gaussian) GetPixel(x uint, y uint, image Image) Color {
 	return Color{0, 0, 0}
 }
 
+func (g Gaussian) PrepareImage(image Image, y_min uint, y_max uint) {
+
+}
+
 type Negatif struct {
 	strength float32
 }
@@ -19,6 +24,10 @@ type Negatif struct {
 func (g Negatif) GetPixel(x uint, y uint, image Image) Color {
 	color := image.GetAt(x, y)
 	return Color{255 - color.r, 255 - color.g, 255 - color.b}
+}
+
+func (g Negatif) PrepareImage(image Image, y_min uint, y_max uint) {
+	
 }
 
 type Froid 
