d5befafd8dc67ff7911c11b940067a1dff23196e
Jean-Baptiste SIMON <72874867+Jbsimon05@users.noreply.github.com>
2025-01-13 15:26:43 +0100

    Ajout filtres (neg prog, froid, chaud, lum)

--- go/filter.go
+++ go/filter.go
@@ -13,12 +13,67 @@ func (g Gaussian) GetPixel(x uint, y uint, image Image) Color {
 }
 
 type Negatif struct {
-	strength float32
+	Strength float32
 }
 
 func (g Negatif) GetPixel(x uint, y uint, image Image) Color {
 	color := image.GetAt(x, y)
-	return Color{255 - color.r, 255 - color.g, 255 - color.b}
+	red := (255-float32(color.R))*g.Strength + float32(color.R)*(1-g.Strength)
+	green := (255-float32(color.G))*g.Strength + float32(color.G)*(1-g.Strength)
+	blue := (255-float32(color.B))*g.Strength + float32(color.B)*(1-g.Strength)
+	return Color{uint8(red), uint8(green), uint8(blue)}
 }
 
-type Froid 
+type Froid struct {
+	Strength float32
+}
+
+func (g Froid) GetPixel(x uint, y uint, image Image) Color {
+	color := image.GetAt(x, y)
+	red := float32(color.R) * (1 - g.Strength)
+	green := float32(color.G) * (1 - g.Strength)
+	blue := float32(color.B) * (1 + g.Strength)
+	if blue > 255 {
+		blue = 255
+	}
+	return Color{uint8(red), uint8(green), uint8(blue)}
+}
+
+type Chaud struct {
+	Strength float32
+}
+
+func (g Chaud) GetPixel(x uint, y uint, image Image) Color {
+	color := image.GetAt(x, y)
+	red := float32(color.R) * (1 + g.Strength)
+	green := float32(color.G) * (1 + g.Strength/2)
+	blue := float32(color.B) * (1 - g.Strength)
+	if red > 255 {
+		red = 255
+	}
+	if green > 255 {
+		green = 255
+	}
+	return Color{uint8(red), uint8(green), uint8(blue)}
+}
+
+type Luminosite struct {
+	Strength float32
+}
+
+func (g Luminosite) GetPixel(x uint, y uint, image Image) Color {
+	color := image.GetAt(x, y)
+	red := float32(color.R) * g.Strength
+	green := float32(color.G) * g.Strength
+	blue := float32(color.B) * g.Strength
+	if red > 255 {
+		red = 255
+	}
+	if green > 255 {
+		green = 255
+	}
+	if blue > 255 {
+		blue = 255
+	}
+	return Color{uint8(red), uint8(green), uint8(blue)}
+}
