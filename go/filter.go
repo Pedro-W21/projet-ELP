41ec1784013f552d51820237be0bf3ffa52a1b89
Jean-Baptiste SIMON <72874867+Jbsimon05@users.noreply.github.com>
2025-01-13 14:20:40 +0100

    merge filter

--- go/filter.go
+++ go/filter.go
@@ -9,12 +9,7 @@ type Gaussian struct {
 }
 
 func (g Gaussian) GetPixel(x uint, y uint, image Image) Color {
-<<<<<<< HEAD
 	return Color{0, 0, 0}
-=======
-	last_color := image.GetAt(x, y)
-	return Color{255 - last_color.R, 255 - last_color.G, 255 - last_color.B}
->>>>>>> fd73357d6428f90ca3e541a90006159225bf54c2
 }
 
 type Negatif struct {
@@ -25,3 +20,5 @@ func (g Negatif) GetPixel(x uint, y uint, image Image) Color {
 	color := image.GetAt(x, y)
 	return Color{255 - color.r, 255 - color.g, 255 - color.b}
 }
+
+type Froid 
