80b686490c3c174d76c0975d0a5e7d711b10f670
Pedro-W21 <89784517+Pedro-W21@users.noreply.github.com>
2025-01-24 10:52:08 +0100

    version qui marche à moitié (plus que pas du tout)

--- elm/src/CheminASvg.elm
+++ elm/src/CheminASvg.elm
@@ -12,11 +12,7 @@ type alias Turtle = {posx:Float, posy:Float, orient:Float}
 
 goForward : List Chemin -> Turtle -> Turtle ->  (List (Svg msg)) -> (Turtle, (List (Svg msg)))
 goForward steps turt next_turt svg_final = 
-    getSvgDataRecursive steps next_turt (List.append svg_final [line [Svg.Attributes.strokeWidth "3", Svg.Attributes.stroke "Black", x1 (String.fromInt(floor(turt.posx))), y1 (String.fromInt(floor(turt.posy))), x2 (String.fromInt(floor(next_turt.posx))), y2 (String.fromInt(floor(next_turt.posy)))] []])
-
-getShapesAndLastTurt :  (List (Svg msg), List Turtle) ->  (List (Svg msg), Turtle)
-getShapesAndLastTurt (shapes, turtles) =
-    (shapes, withDefault (Turtle 0 0 0) (List.head (List.reverse turtles)))
+    getSvgDataRecursive steps next_turt (List.append svg_final [line [Svg.Attributes.strokeWidth "2", Svg.Attributes.stroke "Blue", x1 (String.fromInt(floor(turt.posx))), y1 (String.fromInt(floor(turt.posy))), x2 (String.fromInt(floor(next_turt.posx))), y2 (String.fromInt(floor(next_turt.posy)))] []])
 
 repeatSteps : Int -> List Chemin -> Turtle ->  (List (Svg msg)) -> (Turtle,  (List (Svg msg)))
 repeatSteps nb_left steps turt svg_final = 
@@ -34,7 +30,7 @@ getSvgDataRecursive : List Chemin -> Turtle ->  (List (Svg msg)) -> (Turtle,  (L
 getSvgDataRecursive steps turt final_svg = case steps of
     [] -> (turt, final_svg)
     (step :: rest_of_steps) -> case step of
-                                Forward long -> goForward rest_of_steps (turt) (Turtle (turt.posx + cos(turt.orient) * long) (turt.posy + sin(turt.orient) * long) turt.orient) final_svg
+                                Forward long -> goForward rest_of_steps (turt) (Turtle (turt.posx + cos(degrees turt.orient) * long) (turt.posy + sin(degrees turt.orient) * long) turt.orient) final_svg
                                 Right changement -> getSvgDataRecursive rest_of_steps (Turtle (turt.posx) (turt.posy) (turt.orient + changement)) final_svg
                                 Left changement -> getSvgDataRecursive rest_of_steps (Turtle (turt.posx) (turt.posy) (turt.orient - changement)) final_svg
                                 Repeat nb repeat_steps -> doSvgRecursiveWithTurt rest_of_steps (repeatSteps (nb) (repeat_steps) (turt) (final_svg))
