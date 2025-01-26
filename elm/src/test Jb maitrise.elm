2774c2ff10afdf35d43dc585b5fac98ad578dd08
Jean-Baptiste SIMON <72874867+Jbsimon05@users.noreply.github.com>
2025-01-26 16:59:13 +0100

    début maitrise crayon

---
+++ elm/src/test Jb maitrise.elm
@@ -0,0 +1,67 @@
+module Main exposing (..)
+
+import Browser
+import Html exposing (Html, button, div, text, input)
+import Html.Events exposing (onClick, onInput)
+import Html.Attributes exposing (..)
+import ParseurChemin exposing (Chemin)
+import Svg exposing (..)
+import Svg.Attributes exposing (viewBox, width, height)
+import CheminASvg exposing (..)
+
+-- MAIN
+
+main =
+    Browser.sandbox { init = init, update = update, view = view }
+
+-- MODEL
+
+type alias Model =
+    { commandes : List Chemin
+    , commande_str : String
+    }
+
+init : Model
+init =
+    { commande_str = "", commandes = [] }
+
+-- UPDATE
+
+type Msg
+    = Change String
+    | Render
+
+unwrap : Result (List Parser.DeadEnd) (List Chemin) -> List Chemin
+unwrap res =
+    case res of
+        Ok cool ->
+            cool
+
+        Err _ ->
+            []
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Change str ->
+            { model | commande_str = str }
+
+        Render ->
+            { model | commandes = unwrap (run extraitListeChemin model.commande_str) }
+
+-- VIEW
+
+view : Model -> Html Msg
+view model =
+    div [ style "display" "flex", style "flex-direction" "column", style "align-items" "center", style "justify-content" "center", style "height" "100vh" ]
+        [ div [ style "margin" "10px" ]
+            [ input [ placeholder "Commande à afficher", value model.commande_str, onInput Change, style "padding" "10px", style "font-size" "16px" ] []
+            ]
+        , div [ style "margin" "10px" ]
+            [ button [ onClick Render, style "padding" "10px", style "font-size" "16px" ] [ text "Rendu des commandes" ]
+            ]
+        , div [ style "margin" "10px", style "border" "1px solid #ccc", style "padding" "10px" ]
+            [ svg [ width 300, height 300, viewBox "0 0 300 300" ]
+                (Tuple.second (CheminASvg.getSvgDataRecursive model.commandes (Turtle 150 150 0) []))
+            ]
+        ]
