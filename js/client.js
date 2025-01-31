6ffccec1f6e6cd72b2f3026d5773970b98341c8f
Jean-Baptiste SIMON <72874867+Jbsimon05@users.noreply.github.com>
2025-01-31 15:51:05 +0100

    premier jet clientt

--- js/client.js
+++ js/client.js
@@ -1,7 +1,37 @@
-import net from "net"
+const net = require('net');
+const readline = require('readline');
 
-const client = new net.Socket()
-client.connect({ port: 9999, host: process.argv[2] ?? "localhost" })
-client.on("data", (data) => {
-  console.log(data.toString("utf-8"))
-})
+const client = new net.Socket();
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+client.connect(3000, 'localhost', () => {
+    console.log('Connecté au serveur');
+    rl.question('Entrez votre nom: ', (name) => {
+        client.write(name);
+        startGame();
+    });
+});
+
+function startGame() {
+    rl.question('Appuyez sur Entrée pour continuer...', () => {
+        client.write('ready');
+    });
+}
+
+client.on('data', (data) => {
+    const message = data.toString();
+    console.log(message);
+    if (message.includes('Indices finaux')) {
+        rl.question('Devinez le mot: ', (guess) => {
+            client.write(guess);
+        });
+    }
+});
+
+client.on('close', () => {
+    console.log('Déconnecté du serveur');
+    rl.close();
+});
