8e86b5cd1f057fa5e3ae168cd27df4865a9a7cee
Jean-Baptiste SIMON <72874867+Jbsimon05@users.noreply.github.com>
2025-01-31 15:32:22 +0100

    corr jeu

--- js/jeu.js
+++ js/jeu.js
@@ -1,9 +1,10 @@
 
 console.clear()
 dico = 
-console.log("Bienvenu dans le jeu JustOne!\n");
+console.log("Bienvenue dans le jeu JustOne!\n");
 // pour récupérer le nb de joueurs
 console.log("Combien de joueurs êtes-vous?");
+
 process.stdin.on('data',input=>{
     const nbJoueurs = input.toString().trim(); // nombre de joueurs
     console.log("Très bien, ");
