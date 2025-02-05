3a677e9b7564564113b097899bf390524463ec15
Pedro-W21 <89784517+Pedro-W21@users.noreply.github.com>
2025-02-05 16:00:18 +0100

    rajout d'un saut de ligne

--- js/start.js
+++ js/start.js
@@ -4,7 +4,7 @@ const interface = readline.createInterface({    // Init IHM
     output: process.stdout
 });
 
-interface.question("______________________________________________________________\nClient ou Serveur ? Il faut au moins 2 clients et 1 serveur pour lancer une partie : ", (number) => {
+interface.question("\n______________________________________________________________\nClient ou Serveur ? Il faut au moins 2 clients et 1 serveur pour lancer une partie : ", (number) => {
     if (number == "Serveur") {
         var exec = require("./serveur.js")
     }
