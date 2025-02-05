efdcf849c94e48d9ba040cf5c45efb8807be4c3f
Pedro-W21 <89784517+Pedro-W21@users.noreply.github.com>
2025-02-05 17:13:49 +0100

    fix du start.js pour enlever le doublage de char

--- js/start.js
+++ js/start.js
@@ -3,12 +3,15 @@ const interface = readline.createInterface({    // Init IHM
     input: process.stdin,
     output: process.stdout
 });
+let fichier = ""
 
 interface.question("\n______________________________________________________________\nClient ou Serveur ? Il faut au moins 2 clients et 1 serveur pour lancer une partie : ", (number) => {
+    interface.close()
     if (number == "Serveur") {
-        var exec = require("./serveur.js")
+        fichier = "./serveur.js"
     }
     else {
-        var exec = require("./client.js")
+        fichier = "./client.js"
     }
+    var exec = require(fichier)
 });
